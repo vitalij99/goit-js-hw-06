000ee4fb896cfe4595aa14ccd6ba4fccf70693d6
vitalij99 <vitalisklymko@gmail.com>
2022-12-23 22:24:03 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector("ul#ingredients");
+ const ingredientsList = ingredients.map(e => {
+  const li = document.createElement("li");
+   li.textContent = e
+   li.classList.add("item") 
+    return li
+ });
+  
+list.append(...ingredientsList)
