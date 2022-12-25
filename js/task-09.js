76cd61c5f314bd0878de70ce9b2ecb54f3cb32d7
vitalij99 <vitalisklymko@gmail.com>
2022-12-25 15:43:04 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createColor = document.querySelector(".change-color")
+
+
+createColor.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+})
