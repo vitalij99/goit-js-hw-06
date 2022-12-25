35ed1acdda88da5fad6dad85080b96118002d72d
vitalij99 <vitalisklymko@gmail.com>
2022-12-25 15:29:24 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit)
+
+function onSubmit(event) {    
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені");
+  }
+  else {    
+    console.log("email :" + email.value)
+    console.log("password :" + password.value)
+    event.currentTarget.reset()
+  }          
+}
